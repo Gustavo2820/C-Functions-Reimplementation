31c3ec341a6d5d85240eeeaee9a9f25f5e26a4f5
Gustavo <gugui2820@gmail.com>
2024-06-10 23:04:58 -0300

    modified: comments in src/my_string/my_memcpy.c

--- src/my_string/my_memcpy.c
+++ src/my_string/my_memcpy.c
@@ -2,8 +2,7 @@
 
 void* my_memcpy(void* destiny, void* source, int n) {
 
-    //Cast the void pointers to char pointers,
-    //because character type size is one byte.
+    //We cast the void pointers to char pointers because char type in C is the size of one byte.
     //With this we can iterate over source, byte per byte, and copy to the destiny.
     char* charSource = (char*) source;
     char* charDestiny = (char*) destiny;
